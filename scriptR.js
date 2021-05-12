eafdf7c6a6900a4372db26100cce4e7a38db9284
Goudmant <yvangoudmant@gmail.com>
2021-05-12 11:24:59 +0200

    Radar

---
+++ scriptR.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { Chart } from 'primereact/chart';
+
+export class RadarChartDemo extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.chartData = {
+            labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
+            datasets: [
+                {
+                    label: 'My First dataset',
+                    backgroundColor: 'rgba(179,181,198,0.2)',
+                    borderColor: 'rgba(179,181,198,1)',
+                    pointBackgroundColor: 'rgba(179,181,198,1)',
+                    pointBorderColor: '#fff',
+                    pointHoverBackgroundColor: '#fff',
+                    pointHoverBorderColor: 'rgba(179,181,198,1)',
+                    data: [65, 59, 90, 81, 56, 55, 40]
+                },
+                {
+                    label: 'My Second dataset',
+                    backgroundColor: 'rgba(255,99,132,0.2)',
+                    borderColor: 'rgba(255,99,132,1)',
+                    pointBackgroundColor: 'rgba(255,99,132,1)',
+                    pointBorderColor: '#fff',
+                    pointHoverBackgroundColor: '#fff',
+                    pointHoverBorderColor: 'rgba(255,99,132,1)',
+                    data: [28, 48, 40, 19, 96, 27, 100]
+                }
+            ]
+        };
+
+        this.lightOptions = {
+            legend: {
+                labels: {
+                    fontColor: '#495057'
+                }
+            },
+            scale: {
+                pointLabels: {
+                    fontColor: '#495057'
+                },
+                gridLines: {
+                    color: '#ebedef'
+                }
+            }
+        };
+    }
+
+    render() {
+        return (
+            <div className="card">
+                <Chart type="radar" data={this.chartData} options={this.lightOptions} />
+            </div>
+        )
+    }
+}
